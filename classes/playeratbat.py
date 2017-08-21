bee66ced64948d2814e20e0279eab2f0819bb5ae
Michael Napoleon <michael.napoleon@gmail.com>
2017-08-21 15:47:03 +0100

    Started to work on parsing the pitch data.  Very rough at this point.

---
+++ classes/playeratbat.py
@@ -0,0 +1,36 @@
+class HitResult:
+    SINGLE = '1B'
+    DOUBLE = '2B'
+    TRIPLE = '3B'
+    HOMERUN = 'HR'
+    WALK = 'BB'
+    INTENTIONAL_WALK = 'IBB'
+    HIT_BY_PITCH = 'HB'
+    STRIKEOUT = 'SO'
+    GROUNDOUT = 'GO'
+    GROUND_INTO_DOUBLE_PLAY = 'GO-DP'
+    POPOUT = 'PO'
+    FLYOUT = 'FO'
+    SACRIFICE = 'SAC'
+    REACHED_ON_ERROR = 'ROE'
+
+class PlayerAtBat:
+
+    def __init__(self):
+        self.player = ""
+        self.team = ""
+        self.inning = 0
+        self.pitcher = ""
+        self.balls = 0
+        self.swinging_strikes = 0
+        self.called_strikes = 0
+        self.foul_balls = 0
+        self.ball_in_play = False
+        self.home_run = False
+        self.first_pitch_strike = False
+        self.result = ""
+
+    def __str__(self):
+        return self.player + " - " + self.pitcher + " - Balls: " + str(self.balls) + \
+                " Called Strikes: " + str(self.called_strikes) + " Swinging Stikes: " + str(self.swinging_strikes) + \
+                " First Pitch Strike: " + str(self.first_pitch_strike) + " Result: " + self.result
--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 from bs4 import BeautifulSoup
+from classes.playeratbat import PlayerAtBat
 import re
 import fileinput
 
-
-def process_inning(inning):
+def process_inning(inning, pitcher):
     td_tags  = inning.find_all("td")
+    batter = ""
     for td_tag in td_tags:
         text_data = td_tag.text.strip()
         if text_data.startswith('Pitching'):
-            print(text_data)
+            pitcher = td_tag.find("a").text
         elif text_data.startswith('Batting'):
-            print(text_data)
+            batter = td_tag.find("a").text
         elif text_data=='':
             i = 1
         else:
             at_bat_data = text_data.split("*")
-            for pitch in  at_bat_data:
-                print (pitch)
-
-
-def process_inning2(inning):
-    td_tags  = inning.find_all("td")
-    for td_tag in td_tags:
-        print(td_tag.string)
-
+            print (pitcher + " : " + batter)
+            atbat = PlayerAtBat()
+            atbat.pitcher = pitcher
+            atbat.player = batter
+            for pitch in at_bat_data:
+                if re.match(r'^([0-3]-[0-2]:)', pitch):
+                    pitch_data = re.split(":", pitch)
+                    pitch_count = pitch_data[0]
+                    pitch_outcome = pitch_data[1]
+                    if pitch_outcome.strip() == 'Ball':
+                        atbat.balls = atbat.balls + 1
+                    elif pitch_outcome.strip() == 'Base on Balls':
+                        atbat.balls = atbat.balls + 1
+                        atbat.result = 'BB'
+                    elif pitch_outcome.strip() == 'Called Strike':
+                        atbat.called_strikes = atbat.called_strikes + 1
+                        if pitch_count == '0-0':
+                            atbat.first_pitch_strike = True
+                    elif pitch_outcome.strip() == 'Strikes out  looking':
+                        atbat.called_strikes = atbat.called_strikes + 1
+                        atbat.result = 'SO'
+                    elif pitch_outcome.strip() == 'Swinging Strike':
+                        atbat.swinging_strikes = atbat.swinging_strikes + 1
+                        if pitch_count == '0-0':
+                            atbat.first_pitch_strike = True
+                    elif pitch_outcome.strip() == ' Strikes out  swinging':
+                        atbat.swinging_strikes = atbat.swinging_strikes + 1
+                        atbat.result = 'SO'
+            print(atbat)
 
 
 
@@ -42,5 +63,7 @@ print(home.strip())
 
 innings = soup.find_all("table", class_="data")
 for inning in innings:
-    process_inning(inning)
+    #get the pitcher for the inning
+    pitcher = inning.find_all("th")[1].find("a").text
+    process_inning(inning, pitcher)
 
