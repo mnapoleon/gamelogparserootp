1f5e161d5053e3fbe2f2e6e49627fedf3dd8929f
Michael Napoleon <michael.napoleon@gmail.com>
2019-08-27 21:03:19 -0400

    add some sql as example on how to create the plate discipline stats from the output.

---
+++ sql/batter_player_discipline.sql
@@ -0,0 +1,15 @@
+select
+BatterId,
+Batter,
+round(((sum(sws) + sum(FB) + sum(InP) + SUM(HR)) * 1.0) / (sum(BALLS) + sum(cs) + sum(sws) + sum(FB) + sum(InP) + sum(HR)), 3) as swingPct,
+round(((sum(FB) + sum(InP) + SUM(HR)) * 1.0) / ((sum(sws) + sum(FB) + sum(InP) + SUM(HR)) * 1.0), 3) as contactPct,
+round(sum(sws) * 1.0 / (sum(BALLS) + sum(cs) + sum(sws) + sum(FB) + sum(InP) + sum(HR)), 3) as SwStrPct,
+round(sum(sws) * 1.0 / ((sum(sws) + sum(FB) + sum(InP) + SUM(HR)) * 1.0), 3) as WhiffPct,
+round(sum(fps) * 1.0/ COUNT(BatterId), 3) as FirstPitchStrikePct,
+sum(FPCS) as FirstPitchCalledStrikes,
+sum(FPSS) as FirstPitchSwingingStrikes,
+sum(FPS) as FPS,
+round((sum(FPCS) * 1.0) / sum(Fps), 3) as PctOfCalledFirstPitchStrikes
+from
+logs_table
+group by BatterId
